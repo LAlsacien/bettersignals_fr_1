19eace5f2a5620e8d82bd8c0d269b03dc7f9673e
LAlsacien <97260728+LAlsacien@users.noreply.github.com>
2026-04-01 17:48:16 +0200

    Initial commit

---
+++ mod.lua
@@ -0,0 +1,38 @@
+local function pRequire(modulName)
+    local state, result_or_error = pcall(require, modulName)
+    if state then
+        return result_or_error
+    else
+        return nil, result_or_error
+    end
+end
+
+
+function data()
+    return {
+        info = {
+            name = _("Signalisation Française - Better Signals"),
+            description = _("Test signal LED"),
+            minorVersion = 0,
+            severityAdd = "NONE",
+            severityRemove = "WARNING",
+            authors = { { name = "Moi", role = "CREATOR" } },
+            tags = { "europe", "signal", "better_signals" },
+            dependencies = {
+                "3467585867_1",
+                "3689166005_1",
+            },
+            requiredMods = { { modId = "3689166005_1" } },
+        },
+
+        runFn = function(settings, modParams)
+            local signals = pRequire('nightfury/signals/main')
+            if not signals then return end
+            
+            signals.signals['a_bal_vl_a_s_m_s'] = {
+                type = "main",
+                isAnimated = false,
+            }
+        end,
+    }
+end
